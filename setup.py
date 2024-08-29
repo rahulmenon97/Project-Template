4777c132693dbc07d4e049b21120f9b86af08338
unknown <rahulravimenon97@gmail.com>
2024-08-29 15:14:30 +0400

    project structure updated v1

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="qasystem",
     version="0.0.1",
-    author="sunny",
-    author_email="sunny.savita@ineuron.ai",
+    author="rahulmenon97",
+    author_email="rahulravimenon97@gmail.com",
     packages=find_packages(),
     install_requires=["langchain"]
 )
